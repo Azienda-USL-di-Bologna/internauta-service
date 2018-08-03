81f231b12c010b7456e12af37a0f811e7a4398da
g.demarco <devnull@localhost>
2018-08-03 17:09:13 +0200

    aggiornamento versioni

--- src/main/java/it/bologna/ausl/baborg/service/configuration/authorization/WebSecurityConfig.java
+++ src/main/java/it/bologna/ausl/baborg/service/configuration/authorization/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package it.bologna.ausl.baborg.service.configuration.authorization;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
