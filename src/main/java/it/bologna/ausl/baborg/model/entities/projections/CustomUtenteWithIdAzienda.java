89944a59a164b7c9fa7ef9bd6b19fcc453bc0c1d
f.gusella <devnull@localhost>
2018-09-07 10:46:03 +0200

    set to list..

--- src/main/java/it/bologna/ausl/baborg/model/entities/projections/CustomUtenteWithIdAzienda.java
+++ src/main/java/it/bologna/ausl/baborg/model/entities/projections/CustomUtenteWithIdAzienda.java
@@ -1,11 +1,11 @@
 package it.bologna.ausl.baborg.model.entities.projections;
         
-import it.bologna.ausl.model.entities.baborg.projections.generated.UtenteWithIdAziendaAndPecUtenteSetAndUtenteStrutturaSet;
+import it.bologna.ausl.model.entities.baborg.projections.generated.UtenteWithIdAziendaAndPecUtenteListAndUtenteStrutturaList;
 import java.util.Set;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "CustomUtenteWithIdAzienda", types = it.bologna.ausl.model.entities.baborg.Utente.class)
-public interface CustomUtenteWithIdAzienda extends UtenteWithIdAziendaAndPecUtenteSetAndUtenteStrutturaSet{
+public interface CustomUtenteWithIdAzienda extends UtenteWithIdAziendaAndPecUtenteListAndUtenteStrutturaList{
     
     @Override
 //    @Value("#{@projectionsInterceptorLauncher.lanciaInterceptor(target, 'getIdAzienda', target.getIdAzienda().getClass())}")
@@ -13,9 +13,9 @@ public interface CustomUtenteWithIdAzienda extends UtenteWithIdAziendaAndPecUten
     
     @Override
 //    @Value("#{@projectionsInterceptorLauncher.lanciaInterceptorSet(target, 'getUtenteStrutturaSet')}")
-    public Set getUtenteStrutturaSet();
+    public Set getUtenteStrutturaList();
 
     @Override
 //    @Value("#{@projectionsInterceptorLauncher.lanciaInterceptorSet(target, 'getPecUtenteSet')}")
-    public Set getPecUtenteSet();
+    public Set getPecUtenteList();
 }
