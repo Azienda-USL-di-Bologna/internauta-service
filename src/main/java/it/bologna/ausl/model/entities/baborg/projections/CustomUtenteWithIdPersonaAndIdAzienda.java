f2893baca1c78a9a3bb78d96b9aa59815ee68bb3
a.marcomini <devnull@localhost>
2018-10-11 10:22:48 +0200

    inserimento dei ruoli nella projection tornatat dal login

--- src/main/java/it/bologna/ausl/model/entities/baborg/projections/CustomUtenteWithIdPersonaAndIdAzienda.java
+++ src/main/java/it/bologna/ausl/model/entities/baborg/projections/CustomUtenteWithIdPersonaAndIdAzienda.java
@@ -5,8 +5,11 @@ package it.bologna.ausl.model.entities.baborg.projections;
 
 import it.bologna.ausl.model.entities.baborg.Azienda;
 import it.bologna.ausl.model.entities.baborg.Persona;
+import it.bologna.ausl.model.entities.baborg.Ruolo;
 import it.bologna.ausl.model.entities.baborg.Utente;
 import it.bologna.ausl.model.entities.baborg.projections.generated.UtenteWithIdAziendaAndIdPersona;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.data.rest.core.config.Projection;
 
@@ -18,5 +21,8 @@ public interface CustomUtenteWithIdPersonaAndIdAzienda extends UtenteWithIdAzien
 
     @Override
     public Azienda getIdAzienda();
+
+    @Value("#{@userInfoService.getRuoli(target)}")
+    public List<Ruolo> getRuoli();
 
 }
