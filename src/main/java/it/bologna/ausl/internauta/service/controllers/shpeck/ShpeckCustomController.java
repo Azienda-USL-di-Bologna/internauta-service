3c1e84f3dc2310ec90af5ea9947cd1d0f3a3ba8a
g.russo <devnull@localhost>
2019-04-19 18:52:20 +0200

    Api salvataggio bozza e draft repository

--- src/main/java/it/bologna/ausl/internauta/service/controllers/shpeck/ShpeckCustomController.java
+++ src/main/java/it/bologna/ausl/internauta/service/controllers/shpeck/ShpeckCustomController.java
@@ -2,13 +2,22 @@ package it.bologna.ausl.internauta.service.controllers.shpeck;
 
 import it.bologna.ausl.eml.handler.EmlHandler;
 import it.bologna.ausl.eml.handler.EmlHandlerException;
+import it.bologna.ausl.eml.handler.EmlHandlerUtils;
+import it.bologna.ausl.eml.handler.EmlHandlerAttachment;
+import it.bologna.ausl.internauta.service.repositories.baborg.PecRepository;
+import it.bologna.ausl.internauta.service.repositories.shpeck.DraftRepository;
 import it.bologna.ausl.internauta.service.shpeck.utils.ShpeckCacheableFunctions;
+import it.bologna.ausl.model.entities.baborg.Pec;
+import it.bologna.ausl.model.entities.shpeck.Draft;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -23,10 +32,18 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipOutputStream;
+import javax.mail.Address;
 import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -37,6 +54,12 @@ import org.apache.commons.lang3.tuple.Pair;
 public class ShpeckCustomController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ShpeckCustomController.class);
+    
+    @Autowired
+    private DraftRepository draftRepository;
+    
+    @Autowired
+    private PecRepository pecRepository;
     
     /**
      * 
@@ -97,22 +120,101 @@ public class ShpeckCustomController {
         response.flushBuffer();
     }
     
-    
-//    @RequestMapping(value = "extractMessageData", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<String> extractMessageData(@RequestBody final Message message) throws EmlHandlerException {
-//        return new ResponseEntity(ShpeckCacheableFunctions.getInfoEml(message), HttpStatus.OK);
-//    }
-    
-    
-//        Path filePath = Paths.get("C:\\Users\\Public\\autocertificazione _1__19.pdf").normalize();
-//        Resource resource = new UrlResource(filePath.toUri());
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.parseMediaType("application/pdf"))
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + "autocertificazione _1__19.pdf" + "\"")
-//                .body(resource);
-    
-    
-        //response.addHeader(HttpHeaders.CONTENT_TYPE, );
-        // per farlo scarica
-        // response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=allegati.zip");
+    /**
+     * Salva la bozza della mail sul database
+     * @param idPec L'id dell'indirizzo PEC a cui appartiene la bozza
+     * @param body Il testo html della mail
+     * @param hideRecipients Destinatari nascosti
+     * @param subject L'oggetto della mail
+     * @param from L'indirizzo mittente
+     * @param to Array degli indirizzi di destinazione
+     * @param cc Array degli indirizzi in copia carbone
+     * @param attachments Array degli allegati
+     * @throws AddressException Errore nella creazione degli indirizzi
+     * @throws IOException Errore di salvataggio
+     * @throws MessagingException Errore nella creazione del mimemessage
+     */
+    @Transactional
+    @RequestMapping(value = "saveDraftMessage", method = RequestMethod.POST)
+    public void saveDraftMessage(
+        @RequestParam("idPec") Integer idPec,
+        @RequestParam("body") String body,
+        @RequestParam("hideRecipients") Boolean hideRecipients,
+        @RequestParam("subject") String subject,
+        @RequestParam("from") String from,
+        @RequestParam("to") String[] to,
+        @RequestParam("cc") String[] cc,
+        @RequestParam("attachments") MultipartFile[] attachments
+        ) throws AddressException, IOException, MessagingException {
+        
+        LOG.info("Saving draft message received from PEC with id: " + idPec);
+        LOG.info("Creating the sender address");
+        Address fromAddress = new InternetAddress(from);
+        
+        LOG.info("Creating destination's addresses array");
+        Address toAddress[] = null;
+        if (to != null) {
+            toAddress = new Address[to.length];
+            for (int i = 0; i < to.length; i++) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+        }
+        LOG.info("Creating carbon copy's addresses array");
+        Address ccAddress[] = null;
+        if (cc != null) {
+            ccAddress = new Address[cc.length];
+            for (int i = 0; i < cc.length; i++) {
+                ccAddress[i] = new InternetAddress(cc[i]);
+            }
+        }
+        LOG.info("Creating the attachments array");
+        ArrayList<EmlHandlerAttachment> listAttachments = null;
+        if (attachments != null) {
+            listAttachments = new ArrayList<>();
+            for (MultipartFile attachment : attachments) {
+                EmlHandlerAttachment file = new EmlHandlerAttachment();
+                file.setFileName(attachment.getOriginalFilename());
+                file.setMimeType(attachment.getContentType());
+                file.setFileBytes(attachment.getBytes());
+                listAttachments.add(file);
+            }
+        }
+        
+        LOG.info("Building mime message...");
+        EmlHandlerUtils emlHandlerUtilis = new EmlHandlerUtils();
+        MimeMessage mimeMessage = null;
+        try {
+            mimeMessage = emlHandlerUtilis.buildDraftMessage(body, subject, fromAddress, toAddress, ccAddress, listAttachments);        
+        } catch (MessagingException ex) {
+            LOG.error("Errore while generating the mimemessage", ex);
+            throw new MessagingException("Errore while generating the mimemessage", ex);
+        }
+        LOG.info("Mime message generated correctly!");
+        LOG.info("Preparing the message for saving...");
+        Draft draftMessage = new Draft();
+        try {
+            Pec pec = pecRepository.findById(idPec).orElseThrow();
+            draftMessage.setIdPec(pec);
+            draftMessage.setSubject(subject);
+            draftMessage.setToAddresses(to);
+            draftMessage.setCcAddresses(cc);
+            draftMessage.setHiddenRecipients(Boolean.FALSE);
+            draftMessage.setCreateTime(LocalDateTime.now());
+            draftMessage.setUpdateTime(LocalDateTime.now());
+            draftMessage.setAttachmentsNumber(listAttachments != null ? listAttachments.size() : 0);
+            draftMessage.setAttachmentsName(listAttachments != null ? listAttachments.stream()
+                    .map(EmlHandlerAttachment::getFileName).toArray(size -> new String[size]) : new String[0]);
+            draftMessage.setBody(body);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            mimeMessage.writeTo(baos);
+            draftMessage.setEml(baos.toByteArray());
+            LOG.info("Message ready. Saving...");
+            draftMessage = draftRepository.save(draftMessage);
+        } catch (IOException ex) {
+            LOG.error("Error while saving message");
+            throw new IOException("Error while saving message", ex);
+        } finally {
+            LOG.info("Draft message saved: {}", draftMessage);
+        }
+    }
 }
