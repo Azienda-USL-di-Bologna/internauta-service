3a442683957d507d145662a441d399ab61e9538a
c.toparcean <devnull@localhost>
2019-05-23 15:00:07 +0200

    fix

--- src/main/java/it/bologna/ausl/internauta/service/shpeck/utils/ShpeckCacheableFunctions.java
+++ src/main/java/it/bologna/ausl/internauta/service/shpeck/utils/ShpeckCacheableFunctions.java
@@ -31,47 +31,29 @@ public class ShpeckCacheableFunctions {
 //    private static final Map<String, EmlHandlerResult> concurrentMap = new ConcurrentHashMap();
     private static final Map<String, EmlHandlerResult> concurrentMap = new HashMap<>();
     private static final Map<Integer, Integer> locks = new HashMap<>();
-    private static final Lock lock = new ReentrantLock()
+    private static final Lock lock = new ReentrantLock();
     
     @Cacheable(value = "info_eml", key = "{#emlSource.toString(), #id}", cacheManager = "emlCacheManager")
     public EmlHandlerResult getInfoEml(ShpeckUtils.EmlSource emlSource, Integer id) throws EmlHandlerException, UnsupportedEncodingException, BadParamsException, IOException {
-//        synchronized (concurrentMap.computeIfAbsent(emlSource.toString() + "_" + id, k -> {
-        //concurrentMap.put(id.toString(), new Object());
-        
-        Integer res = locks.get(id);
-        if (res != null) {
-            synchronized (lock.) {
-                locks.put(id, id);
-                // cose
-            }
-        } else {
-                locks.put(id, id);
-                // cose
-            
-        }
-        
-        
-        if (res != null) {
-            synchronized (lock) {
-                if (concurrentMap.get(id.toString()) == null) {
+
                     File downloadedEml = null;
                     EmlHandlerResult handledEml = null;
                     try {
                         downloadedEml = shpeckUtils.downloadEml(emlSource, id);
                         handledEml = EmlHandler.handleEml(downloadedEml.getAbsolutePath());
                     }
-                    catch (BadParamsException | IOException | EmlHandlerException ex) {
-                        Logger.getLogger(ShpeckCacheableFunctions.class.getName()).log(Level.SEVERE, null, ex);
-                        return null ;
-                    } finally {
+//                    catch (BadParamsException | IOException | EmlHandlerException ex) {
+//                        Logger.getLogger(ShpeckCacheableFunctions.class.getName()).log(Level.SEVERE, null, ex);
+//                        return null ;
+//                    } 
+                    finally {
                         if (downloadedEml != null && downloadedEml.exists()) {
                             downloadedEml.delete();
                         }
                     }
-                    concurrentMap.put(id.toString(), handledEml);
-                }
-            }
-        }
-        return concurrentMap.get(emlSource.toString() + "_" + id);
+//                    concurrentMap.put(id.toString(), handledEml);
+                return  handledEml;
+                
+            
     }
 }
