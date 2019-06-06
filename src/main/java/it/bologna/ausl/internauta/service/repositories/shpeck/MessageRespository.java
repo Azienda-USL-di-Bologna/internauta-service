af986c863c960df6bb1b5977cbcf86024b4a0c4e
c.toparcean <devnull@localhost>
2019-06-06 17:46:39 +0200

    reindirizzare un messaggio

--- src/main/java/it/bologna/ausl/internauta/service/repositories/shpeck/MessageRespository.java
+++ src/main/java/it/bologna/ausl/internauta/service/repositories/shpeck/MessageRespository.java
@@ -7,14 +7,21 @@ import it.nextsw.common.annotations.NextSdrRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import it.nextsw.common.repositories.NextSdrQueryDslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 @NextSdrRepository(repositoryPath = "${shpeck.mapping.url.root}/message", defaultProjection = MessageWithPlainFields.class)
 @RepositoryRestResource(collectionResourceRel = "message", path = "message", exported = false, excerptProjection = MessageWithPlainFields.class)
 public interface MessageRespository extends
-        NextSdrQueryDslRepository<Message, Integer, QMessage>, 
+        NextSdrQueryDslRepository<Message, Integer, QMessage>,
         JpaRepository<Message, Integer>,
-        CrudRepository<Message, Integer>{
+        CrudRepository<Message, Integer> {
+
+    @Procedure("shpeck.get_id_azienda_repository")
+    public Integer getIdAziendaRepository(
+            @Param("id_message") Integer idMessage
+    );
 
 //    @Override
 //    default void customize(QuerydslBindings bindings, QMessage entityPath) {
@@ -25,6 +32,4 @@ public interface MessageRespository extends
 //             return null;
 //         });
 //    }
-    
-    
 }
