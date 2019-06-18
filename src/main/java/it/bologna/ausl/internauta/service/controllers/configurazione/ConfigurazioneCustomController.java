cad591caa3cece5494bb4da9ef899d579f21c4b2
g.demarco <devnull@localhost>
2019-06-18 13:54:39 +0200

    fix salvataggio impostazioni

--- src/main/java/it/bologna/ausl/internauta/service/controllers/configurazione/ConfigurazioneCustomController.java
+++ src/main/java/it/bologna/ausl/internauta/service/controllers/configurazione/ConfigurazioneCustomController.java
@@ -9,6 +9,8 @@ import it.bologna.ausl.internauta.service.authorization.AuthenticatedSessionData
 import it.bologna.ausl.internauta.service.repositories.baborg.PersonaRepository;
 import it.bologna.ausl.internauta.service.repositories.configurazione.ApplicazioneRepository;
 import it.bologna.ausl.internauta.service.repositories.configurazione.ImpostazioniApplicazioniRepository;
+import it.bologna.ausl.model.entities.baborg.Persona;
+import it.bologna.ausl.model.entities.baborg.Utente;
 import it.bologna.ausl.model.entities.configuration.ImpostazioniApplicazioni;
 import it.bologna.ausl.model.entities.configuration.QImpostazioniApplicazioni;
 import java.util.Map;
@@ -47,13 +49,26 @@ public class ConfigurazioneCustomController {
     @Transactional(rollbackFor = {Exception.class, Error.class})
     public void setImpostazioniApplicazioni(@RequestBody Map impostazioniVisualizzazione) throws JsonProcessingException, BlackBoxPermissionException {
         AuthenticatedSessionData authenticatedUserProperties = authenticatedSessionDataBuilder.getAuthenticatedUserProperties();
-        LOGGER.info(String.format("realPerson: %s", objectMapper.writeValueAsString(authenticatedUserProperties.getRealPerson())));
-        LOGGER.info(String.format("realUser: %s", objectMapper.writeValueAsString(authenticatedUserProperties.getRealUser())));
+        Persona person;
+        Utente user;
         if (authenticatedUserProperties.getRealPerson() != null) {
-            LOGGER.info(String.format("applicazione realPerson: %s", authenticatedUserProperties.getRealPerson().getApplicazione()));
+            LOGGER.info("si real user");
+            person = authenticatedUserProperties.getRealPerson();
+            user = authenticatedUserProperties.getRealUser();
+        } else {
+            LOGGER.info("no real user");
+            person = authenticatedUserProperties.getPerson();
+            user = authenticatedUserProperties.getUser();
         }
-        BooleanExpression impostazioniFilter = QImpostazioniApplicazioni.impostazioniApplicazioni.idApplicazione.id.eq(authenticatedUserProperties.getRealPerson().getApplicazione())
-                .and(QImpostazioniApplicazioni.impostazioniApplicazioni.idPersona.id.eq(authenticatedUserProperties.getRealPerson().getId()));
+        LOGGER.info(String.format("person: %s", objectMapper.writeValueAsString(person)));
+        LOGGER.info(String.format("user: %s", objectMapper.writeValueAsString(user)));
+        LOGGER.info(String.format("applicazione: %s", person.getApplicazione()));
+
+        BooleanExpression impostazioniFilter = QImpostazioniApplicazioni.impostazioniApplicazioni.idApplicazione.id
+                .eq(person.getApplicazione())
+                .and(QImpostazioniApplicazioni.impostazioniApplicazioni.idPersona.id
+                        .eq(person.getId()));
+
         Optional<ImpostazioniApplicazioni> impostazioniOp = this.impostazioniApplicazioniRepository.findOne(impostazioniFilter);
         ImpostazioniApplicazioni impostazioni;
         if (impostazioniOp.isPresent()) {
@@ -61,8 +76,8 @@ public class ConfigurazioneCustomController {
         }
         else {
             impostazioni = new ImpostazioniApplicazioni();
-            impostazioni.setIdApplicazione(applicazioneRepository.getOne(authenticatedUserProperties.getRealPerson().getApplicazione()));
-            impostazioni.setIdPersona(personaRepository.getOne(authenticatedUserProperties.getRealPerson().getId()));
+            impostazioni.setIdApplicazione(applicazioneRepository.getOne(person.getApplicazione()));
+            impostazioni.setIdPersona(personaRepository.getOne(person.getId()));
         }
         impostazioni.setImpostazioniVisualizzazione(objectMapper.writeValueAsString(impostazioniVisualizzazione));
         this.impostazioniApplicazioniRepository.save(impostazioni);
