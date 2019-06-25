440caa2c3e370cac2118f2d8ffbd794284d8e654
g.demarco <devnull@localhost>
2019-06-25 17:10:27 +0200

    ordinamento tag

---
+++ src/main/java/it/bologna/ausl/model/entities/shpeck/projections/CustomPecWithFolderListAndPecAziendaListAndTagList.java
@@ -0,0 +1,25 @@
+package it.bologna.ausl.model.entities.shpeck.projections;
+
+import it.bologna.ausl.model.entities.baborg.Pec;
+import it.bologna.ausl.model.entities.baborg.projections.generated.PecWithFolderListAndPecAziendaListAndTagList;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+/**
+ *
+ * @author Giuseppe Russo <g.russo@nsi.it>
+ */
+
+@Projection(name = "CustomPecWithFolderListAndPecAziendaListAndTagList", types = Pec.class)
+public interface CustomPecWithFolderListAndPecAziendaListAndTagList extends PecWithFolderListAndPecAziendaListAndTagList {
+        
+    @Value("#{@projectionsInterceptorLauncher.lanciaInterceptorCollection(target, 'getTagList', @projectionsInterceptorLauncher.buildSort('asc', 'type', 'description'))}")
+    @Override
+    public Object getTagList();
+
+//    @Value("#{@projectionsInterceptorLauncher.lanciaInterceptorCollection(target, 'getFolderList', @projectionsInterceptorLauncher.buildSort('asc', 'type', 'description'))}")
+//    @Override
+//    public Object getFolderList();
+    
+    
+}
