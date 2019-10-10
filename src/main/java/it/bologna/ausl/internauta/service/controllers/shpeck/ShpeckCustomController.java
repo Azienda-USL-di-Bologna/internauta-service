5d759a3d2ede2aa04e8502aa5e050b02d85aa11a
g.zoli <devnull@localhost>
2019-10-10 12:00:59 +0200

    nella manageMessageRegistration: considerati messaggi fratelli che appartengono a caselle che appartengono anche all'azienda su cui sto lavorando

--- src/main/java/it/bologna/ausl/internauta/service/controllers/shpeck/ShpeckCustomController.java
+++ src/main/java/it/bologna/ausl/internauta/service/controllers/shpeck/ShpeckCustomController.java
@@ -90,6 +90,7 @@ import it.bologna.ausl.internauta.service.repositories.shpeck.MessageFolderRepos
 import it.bologna.ausl.internauta.service.repositories.shpeck.MessageCompleteRepository;
 import it.bologna.ausl.internauta.service.repositories.shpeck.FolderRepository;
 import it.bologna.ausl.internauta.service.utils.InternautaConstants;
+import it.bologna.ausl.model.entities.baborg.Azienda;
 import it.bologna.ausl.model.entities.logs.OperazioneKrint;
 import it.bologna.ausl.model.entities.baborg.PecAzienda;
 import it.bologna.ausl.model.entities.shpeck.QMessage;
@@ -685,12 +686,11 @@ public class ShpeckCustomController implements ControllerHandledExceptions {
             // recupero tutti i messaggi con quell uuid
             List<Message> messagesByUuid = messageRepository.findByUuidMessage(StringUtils.trimWhitespace(uuidMessage));
             List<Message> messages = new ArrayList();
-            // Dei messagesByUuid trovati tengo solo quelli che appartengono a caselle che appartengono solo all'azienda su cui sto lavorando.
+            // Dei messagesByUuid trovati tengo solo quelli che appartengono a caselle che appartengono anche all'azienda su cui sto lavorando.
             for (Message message : messagesByUuid) {
-                List<PecAzienda> pecAziendaList = message.getIdPec().getPecAziendaList();
+                List<Integer> idAziendaList = message.getIdPec().getPecAziendaList().stream().map(pa -> pa.getIdAzienda().getId()).collect(Collectors.toList());
                 if (message.getId().equals(idMessage)
-                        || pecAziendaList.isEmpty()
-                        || (pecAziendaList.size() == 1 && pecAziendaList.get(0).getIdAzienda().getId().equals(authenticatedUserProperties.getUser().getIdAzienda().getId()))) {
+                        || (idAziendaList.size() >= 1 && idAziendaList.contains(authenticatedUserProperties.getUser().getIdAzienda().getId()))) {
                     messages.add(message);
                 }
             }
