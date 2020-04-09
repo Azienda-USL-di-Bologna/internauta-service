149dcf4966be944cb56758b3a87d77cb78fd362a
c.toparcean <devnull@localhost>
2020-04-09 12:30:23 +0200

    add custom rubrica projection CustomContattoAllWithGruppoExpanded

---
+++ src/main/java/it/bologna/ausl/model/entities/rubrica/projections/CustomContattoAllWithGruppoExpanded.java
@@ -0,0 +1,21 @@
+package it.bologna.ausl.model.entities.rubrica.projections;
+
+import it.bologna.ausl.model.entities.rubrica.Contatto;
+import it.bologna.ausl.model.entities.rubrica.projections.generated.ContattoWithContattiDelGruppoListAndEmailListAndIdPersonaCreazioneAndIndirizziListAndTelefonoList;
+import it.bologna.ausl.model.entities.rubrica.projections.generated.GruppiContattiWithIdContattoAndIdDettaglioContatto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+/**
+ *
+ * @author gus
+ */
+@Projection(name = "CustomContattoAllWithGruppoExpanded", types = Contatto.class)
+public interface CustomContattoAllWithGruppoExpanded extends ContattoWithContattiDelGruppoListAndEmailListAndIdPersonaCreazioneAndIndirizziListAndTelefonoList {
+
+    @Value("#{@projectionBeans.getGruppiContattiWithIdContattoAndIdDettaglioContatto(target)}")
+    @Override
+    public List<GruppiContattiWithIdContattoAndIdDettaglioContatto> getContattiDelGruppoList();
+}
+// contatto -> gruppi_contatto(contatti del gruppo) -> dettaglictonatto & contatto
