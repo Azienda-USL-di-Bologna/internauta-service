d70703921632c1dc57dc02bc4503f97392f4848e
g.demarco <devnull@localhost>
2020-12-03 12:20:55 +0100

    usata nuova projection

--- src/main/java/it/bologna/ausl/model/entities/rubrica/projections/CustomGruppiContattiWithDettaglioContattoExpanded.java
+++ src/main/java/it/bologna/ausl/model/entities/rubrica/projections/CustomGruppiContattiWithDettaglioContattoExpanded.java
@@ -4,7 +4,7 @@ import it.bologna.ausl.internauta.utils.bds.types.PermessoEntitaStoredProcedure;
 import it.bologna.ausl.model.entities.rubrica.GruppiContatti;
 import it.bologna.ausl.model.entities.rubrica.projections.generated.ContattoWithContattiDelGruppoListAndIdPersonaCreazioneAndIdUtenteCreazione;
 import it.bologna.ausl.model.entities.rubrica.projections.generated.DettaglioContattoWithEmailAndIndirizzoAndTelefono;
-import it.bologna.ausl.model.entities.rubrica.projections.generated.GruppiContattiWithIdDettaglioContattoAndIdGruppo;
+import it.bologna.ausl.model.entities.rubrica.projections.generated.GruppiContattiWithIdContattoAndIdDettaglioContattoAndIdGruppo;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
@@ -14,7 +14,7 @@ import org.springframework.data.rest.core.config.Projection;
  * @author gus
  */
 @Projection(name = "CustomGruppiContattiWithDettaglioContattoExpanded", types = GruppiContatti.class)
-public interface CustomGruppiContattiWithDettaglioContattoExpanded extends GruppiContattiWithIdDettaglioContattoAndIdGruppo {
+public interface CustomGruppiContattiWithDettaglioContattoExpanded extends GruppiContattiWithIdContattoAndIdDettaglioContattoAndIdGruppo {
 
     @Override
     @Value("#{@projectionsInterceptorLauncher.lanciaInterceptor(target, 'getIdDettaglioContatto', 'DettaglioContattoWithEmailAndIndirizzoAndTelefono')}")
