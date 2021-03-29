4e2263e254c5945214004590b1b79846e08582c9
a.marcomini <devnull@localhost>
2021-03-29 16:24:20 +0200

    inserimento document-generator

--- src/main/java/it/bologna/ausl/internauta/service/controllers/scripta/ScriptaCustomController.java
+++ src/main/java/it/bologna/ausl/internauta/service/controllers/scripta/ScriptaCustomController.java
@@ -5,6 +5,8 @@
  */
 package it.bologna.ausl.internauta.service.controllers.scripta;
 
+import it.bologna.ausl.documentgenerator.GeneratePE;
+import it.bologna.ausl.documentgenerator.exceptions.HttpInternautaResponseException;
 import it.bologna.ausl.internauta.service.configuration.utils.ReporitoryConnectionManager;
 import it.bologna.ausl.internauta.service.exceptions.http.Http500ResponseException;
 import it.bologna.ausl.internauta.service.repositories.scripta.AllegatoRepository;
@@ -12,20 +14,22 @@ import it.bologna.ausl.internauta.service.repositories.scripta.DocRepository;
 import it.bologna.ausl.minio.manager.MinIOWrapper;
 import it.bologna.ausl.minio.manager.MinIOWrapperFileInfo;
 import it.bologna.ausl.minio.manager.exceptions.MinIOWrapperException;
+import it.bologna.ausl.model.entities.baborg.Utente;
 import it.bologna.ausl.model.entities.scripta.Allegato;
 import it.bologna.ausl.model.entities.scripta.Doc;
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import javax.mail.Part;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.FilenameUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -53,6 +57,9 @@ public class ScriptaCustomController {
 
     @Autowired
     AllegatoRepository allegatoRepository;
+
+    @Autowired
+    GeneratePE generatePE;
 
     @RequestMapping(value = "saveAllegato", method = RequestMethod.POST)
     public ResponseEntity<List<Allegato>> saveAllegato(
@@ -110,6 +117,30 @@ public class ScriptaCustomController {
 
         return ResponseEntity.ok(savedFilesOnInternauta);
 
+    }
+
+    @RequestMapping(value = "createPE", method = RequestMethod.POST)
+    public String createPE(
+            @RequestParam("id_doc") Integer idDoc,
+            HttpServletRequest request) throws HttpInternautaResponseException, Throwable {
+
+        Optional<Doc> doc = docRepository.findById(idDoc);
+
+        if (doc.isEmpty()) {
+            return null;
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Utente loggedUser = (Utente) authentication.getPrincipal();
+
+        // TODO: oggetto dei parametri poi tradotto in string. trasformare i json parametri in mappa
+        JSONObject jSONparametri = new JSONObject();
+
+        // TODO: trasformare i file in MultipartFile e passarli
+        // vedere: https://stackoverflow.com/questions/54712810/java-spring-best-way-to-convert-a-file-to-a-multipartfile
+        generatePE.init(loggedUser.getIdPersona().getCodiceFiscale(), null, null, null);
+        String record = generatePE.create();
+
+        return record;
     }
 
     @Transactional(rollbackFor = Throwable.class)
