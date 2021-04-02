3f2912e917dea922eaacd27f4d65d1a112ee34d4
f.gusella <devnull@localhost>
2021-04-02 10:55:02 +0200

    projection per scripta

--- src/main/java/it/bologna/ausl/model/entities/scripta/projections/DocWithAll.java
+++ src/main/java/it/bologna/ausl/model/entities/scripta/projections/DocWithAll.java
@@ -3,6 +3,7 @@ package it.bologna.ausl.model.entities.scripta.projections;
 import it.bologna.ausl.model.entities.scripta.Doc;
 import it.bologna.ausl.model.entities.scripta.Related;
 import it.bologna.ausl.model.entities.scripta.projections.generated.DocWithAllegatiAndCoinvoltiAndCompetentiAndIdAziendaAndIdPersonaCreazioneAndMittentiAndRelated;
+import it.bologna.ausl.model.entities.scripta.projections.generated.RelatedWithSpedizioneList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
@@ -16,8 +17,8 @@ public interface DocWithAll extends DocWithAllegatiAndCoinvoltiAndCompetentiAndI
 
     @Override
 //    @Value("#{target.getRelated().stream().filter(r -> r.getTipo().toString() == Related.TipoRelated.MITTENTE)}")
-    @Value("#{@projectionBeans.filterRelated(target.getRelated(), 'MITTENTE')}")
-    public List<Related> getMittenti();
+    @Value("#{@projectionBeans.filterRelatedWithSpedizioneList(target.getRelated(), 'MITTENTE')}")
+    public List<CustomRelatedWithSpedizioneList> getMittenti();
 
     @Override
     @Value("#{@projectionBeans.filterRelated(target.getRelated(), 'A')}")
