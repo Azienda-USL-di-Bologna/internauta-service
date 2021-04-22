f050d85f1d0bfb828a5c7908ef322069c732a522
m.donza <devnull@localhost>
2021-04-22 17:18:26 +0200

    gestione allegati download e delete

--- src/main/java/it/bologna/ausl/internauta/service/interceptors/scripta/AllegatoInterceptor.java
+++ src/main/java/it/bologna/ausl/internauta/service/interceptors/scripta/AllegatoInterceptor.java
@@ -1,15 +1,20 @@
 package it.bologna.ausl.internauta.service.interceptors.scripta;
 
+import it.bologna.ausl.internauta.service.configuration.nextsdr.RestControllerEngineImpl;
 import it.bologna.ausl.internauta.service.configuration.utils.ReporitoryConnectionManager;
 import it.bologna.ausl.internauta.service.interceptors.InternautaBaseInterceptor;
 import it.bologna.ausl.internauta.service.repositories.scripta.AllegatoRepository;
+import it.bologna.ausl.internauta.service.repositories.scripta.DettaglioAllegatoRepository;
 import it.bologna.ausl.minio.manager.MinIOWrapper;
 import it.bologna.ausl.minio.manager.exceptions.MinIOWrapperException;
 import it.bologna.ausl.model.entities.scripta.Allegato;
 import it.bologna.ausl.model.entities.scripta.DettaglioAllegato;
 import it.nextsw.common.annotations.NextSdrInterceptor;
+import it.nextsw.common.controller.exceptions.NotFoundResourceException;
+import it.nextsw.common.controller.exceptions.RestControllerEngineException;
 import it.nextsw.common.interceptors.exceptions.AbortSaveInterceptorException;
 import it.nextsw.common.interceptors.exceptions.SkipDeleteInterceptorException;
+import it.nextsw.common.repositories.NextSdrQueryDslRepository;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -17,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -30,51 +36,60 @@ public class AllegatoInterceptor extends InternautaBaseInterceptor {
     private static final Logger LOGGER = LoggerFactory.getLogger(AllegatoInterceptor.class);
 
     @Autowired
+    private RestControllerEngineImpl restControllerEngine;
+    
+    @Autowired
     ReporitoryConnectionManager aziendeConnectionManager;
 
     @Autowired
     AllegatoRepository allegatoRepository;
+    
+    @Autowired
+    DettaglioAllegatoRepository dettaglioAllegatoRepository;
+    
+    
+    @Autowired
+    @Qualifier(value = "customRepositoryEntityMap")
+    protected Map<String, NextSdrQueryDslRepository> customRepositoryEntityMap;
 
     @Override
     public Class getTargetEntityClass() {
         return Allegato.class;
     }
 
+//    @Override
+//    public void beforeDeleteEntityInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request, boolean mainEntity, Class projectionClass) throws AbortSaveInterceptorException, SkipDeleteInterceptorException {
+//        Allegato allegato = (Allegato) entity;
+//        MinIOWrapper minIOWrapper = aziendeConnectionManager.getMinIOWrapper();
+//        List<DettaglioAllegato> dettagliAllegatiList = allegato.getDettagliAllegatiList();
+//        for (DettaglioAllegato dettaglioAllegato : dettagliAllegatiList) {
+//            try {
+//                
+//                restControllerEngine.delete(dettaglioAllegato.getId(), request, additionalData, null, false, projectionClass.getSimpleName(), dettaglioAllegatoRepository);
+//            } catch (Exception ex) {
+//                LOGGER.error("errore nell'eliminazione dei dettagli allegati",ex);
+//                throw new AbortSaveInterceptorException("errore nell'eliminazione dei dettagli allegati",ex);
+//            }
+//        }
+//
+//    }
+
     @Override
     public void afterDeleteEntityInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request, boolean mainEntity, Class projectionClass) throws AbortSaveInterceptorException, SkipDeleteInterceptorException {
-        /*
-            Sto cancellando un allegato, dunque:
-            - Se è un allegato contenitore devo cancellare i file correlati e anche in questo caso dal repository 
-            - Devo cancellare il file dal repository
-            - Un trigger si occupa di aggiustare la numerazione degli allegati
-         */
+//        /*
+//            Sto cancellando un allegato, dunque:
+//            - Se è un allegato contenitore devo cancellare i file correlati e anche in questo caso dal repository 
+//            - Devo cancellare il file dal repository
+//            - Un trigger si occupa di aggiustare la numerazione degli allegati
+//         */
         Allegato allegato = (Allegato) entity;
         MinIOWrapper minIOWrapper = aziendeConnectionManager.getMinIOWrapper();
 
         // Cancello gli eventuali allegati figli.
         List<Allegato> allegatiFigliList = allegato.getAllegatiFigliList();
         for (Allegato a : allegatiFigliList) {
-            try {
-                List<DettaglioAllegato> dettagliAllegatiList = a.getDettagliAllegatiList();
-                for (DettaglioAllegato da : dettagliAllegatiList) {
-                    minIOWrapper.deleteByFileId(da.getIdRepository());
-                }
-            } catch (MinIOWrapperException ex) {
-                java.util.logging.Logger.getLogger(AllegatoInterceptor.class.getName()).log(Level.SEVERE, null, ex);
-            }
             allegatoRepository.delete(a);
         }
-
-        // Cancello dal repository l'allegato
-        try {
-            List<DettaglioAllegato> dettagliAllegatiList = allegato.getDettagliAllegatiList();
-                for (DettaglioAllegato da : dettagliAllegatiList) {
-                    minIOWrapper.deleteByFileId(da.getIdRepository());
-                }
-        } catch (MinIOWrapperException ex) {
-            java.util.logging.Logger.getLogger(AllegatoInterceptor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
         super.afterDeleteEntityInterceptor(entity, additionalData, request, mainEntity, projectionClass); //To change body of generated methods, choose Tools | Templates.
     }
 }
