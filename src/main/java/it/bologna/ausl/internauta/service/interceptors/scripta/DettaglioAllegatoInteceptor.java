64201b82bd74937bd1b3d7d589c5030b15bff91a
m.donza <devnull@localhost>
2021-05-04 09:44:07 +0200

    rm 55960

--- src/main/java/it/bologna/ausl/internauta/service/interceptors/scripta/DettaglioAllegatoInteceptor.java
+++ src/main/java/it/bologna/ausl/internauta/service/interceptors/scripta/DettaglioAllegatoInteceptor.java
@@ -9,14 +9,11 @@ import it.bologna.ausl.internauta.service.configuration.utils.ReporitoryConnecti
 import it.bologna.ausl.internauta.service.interceptors.InternautaBaseInterceptor;
 import it.bologna.ausl.internauta.service.repositories.scripta.AllegatoRepository;
 import it.bologna.ausl.minio.manager.MinIOWrapper;
-import it.bologna.ausl.minio.manager.exceptions.MinIOWrapperException;
-import it.bologna.ausl.model.entities.scripta.Allegato;
 import it.bologna.ausl.model.entities.scripta.DettaglioAllegato;
 import it.nextsw.common.annotations.NextSdrInterceptor;
 import it.nextsw.common.interceptors.exceptions.AbortSaveInterceptorException;
 import it.nextsw.common.interceptors.exceptions.SkipDeleteInterceptorException;
 import java.util.Map;
-import java.util.logging.Level;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,7 +43,7 @@ public class DettaglioAllegatoInteceptor extends InternautaBaseInterceptor {
     @Override
     public void beforeDeleteEntityInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request, boolean mainEntity, Class projectionClass) throws AbortSaveInterceptorException, SkipDeleteInterceptorException {
         DettaglioAllegato dettaglioAllegato = (DettaglioAllegato) entity;
-        //devo eliminare i file su minIO forse è meglio farlo nell'after delete?
+        //TODO: in caso di fallimento quando si hanno piu dettagli allegati se uno fallisce bisogna fare l'undelete degli altri
         MinIOWrapper minIOWrapper = aziendeConnectionManager.getMinIOWrapper();
         try {
             minIOWrapper.deleteByFileId(dettaglioAllegato.getIdRepository());
