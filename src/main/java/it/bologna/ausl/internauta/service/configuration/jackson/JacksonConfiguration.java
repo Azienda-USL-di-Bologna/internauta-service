efcce8285736bb23f0234f2a588e6397bd8c6814
lazzarini <devnull@localhost>
2021-06-11 17:49:09 +0200

    fix localdate e localdatetime

--- src/main/java/it/bologna/ausl/internauta/service/configuration/jackson/JacksonConfiguration.java
+++ src/main/java/it/bologna/ausl/internauta/service/configuration/jackson/JacksonConfiguration.java
@@ -1,5 +1,7 @@
 package it.bologna.ausl.internauta.service.configuration.jackson;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,9 @@ public class JacksonConfiguration {
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
 //        SimpleModule s = new SimpleModule();
 //        s.addDeserializer(ZonedDateTime.class, new ZoneDateTimeDeserializer(ZonedDateTime.class));
-    return builder -> builder.deserializerByType(ZonedDateTime.class, new ZoneDateTimeDeserializer(ZonedDateTime.class));
-}
+        return builder -> builder
+                .deserializerByType(ZonedDateTime.class, new ZoneDateTimeDeserializer(ZonedDateTime.class))
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(LocalDateTime.class))
+                .deserializerByType(LocalDate.class, new LocalDateDeserializer(LocalDate.class));
+    }
 }
