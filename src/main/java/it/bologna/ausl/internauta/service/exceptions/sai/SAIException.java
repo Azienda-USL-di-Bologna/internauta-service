ce2a43986892abef274559f3f071bd69bc372d10
l.salomone <devnull@localhost>
2021-10-12 10:11:59 +0200

    Eccezzioni SAI

---
+++ src/main/java/it/bologna/ausl/internauta/service/exceptions/sai/SAIException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.bologna.ausl.internauta.service.exceptions.sai;
+
+/**
+ *
+ * @author Salo
+ */
+public abstract class SAIException extends Exception {
+
+}
--- src/test/java/it/bologna/ausl/internauta/service/test/schedulers/FascicolatoreOutboxGediWorkerTest.java
+++ src/test/java/it/bologna/ausl/internauta/service/test/schedulers/FascicolatoreOutboxGediWorkerTest.java
@@ -38,7 +38,7 @@ public class FascicolatoreOutboxGediWorkerTest {
             log.info("Loggo");
             mittente = "babel.test1@pec.ausl.bologna.it";
             cf = "SLMLNZ85C13A944M";
-            //numerazioneGerarchica = "56/2021";
+            numerazioneGerarchica = "56-1/2021";
             FascicolatoreAutomaticoGediParams fascicolatoreAutomaticoGediParams = new FascicolatoreAutomaticoGediParams(1, idAzienda, cf, mittente, numerazioneGerarchica);
             worker.setParams(fascicolatoreAutomaticoGediParams);
             worker.run();
