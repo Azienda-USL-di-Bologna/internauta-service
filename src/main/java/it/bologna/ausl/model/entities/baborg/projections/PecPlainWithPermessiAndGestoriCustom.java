a49d663a6b74d71806d6eb6dbb65c6a036704463
lazzarini <devnull@localhost>
2021-11-10 17:52:17 +0100

    fix a gestori pec

--- src/main/java/it/bologna/ausl/model/entities/baborg/projections/PecPlainWithPermessiAndGestoriCustom.java
+++ src/main/java/it/bologna/ausl/model/entities/baborg/projections/PecPlainWithPermessiAndGestoriCustom.java
@@ -3,6 +3,7 @@ package it.bologna.ausl.model.entities.baborg.projections;
 import it.bologna.ausl.internauta.utils.bds.types.PermessoEntitaStoredProcedure;
 import it.bologna.ausl.model.entities.baborg.Pec;
 import it.bologna.ausl.model.entities.baborg.Persona;
+import it.bologna.ausl.model.entities.baborg.projections.generated.PersonaWithPlainFields;
 import java.util.List;
 import org.springframework.data.rest.core.config.Projection;
 
@@ -12,8 +13,8 @@ import org.springframework.data.rest.core.config.Projection;
  */
 @Projection(name = "PecPlainWithPermessiAndGestoriCustom", types = Pec.class)
 public interface PecPlainWithPermessiAndGestoriCustom extends PecWithPecProviderAndAziendaCustom {
+
     public List<PermessoEntitaStoredProcedure> getPermessi();
-    
-    @Override
-    public List<Persona> getGestori();
+
+    public List<PersonaWithPlainFields> getGestori();
 }
