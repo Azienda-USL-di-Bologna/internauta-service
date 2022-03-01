c16918a502f06c4c5c76733c85015ebffa64ffd2
l.salomone <devnull@localhost>
2022-03-01 12:26:36 +0100

    Caricamento idVicari con projection custom

---
+++ src/test/java/it/bologna/ausl/internauta/service/test/schedulers/SalTestInviaNotificheWorker.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.bologna.ausl.internauta.service.test.schedulers;
+
+import it.bologna.ausl.internauta.service.schedulers.managers.InviaNotificaAttivitaSospeseWorkerManager;
+import java.util.Date;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Salo
+ */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SalTestInviaNotificheWorker {
+
+//    @Autowired
+//    InviaNotificaAttivitaSospeseWorkerManager worker;
+//
+//    @Test
+//    public void test() {
+//        Date startTime = new Date();
+//        worker.run();
+//        Date endTime = new Date();
+//        long seconds = (endTime.getTime()-startTime.getTime())/1000;
+//        System.out.println("Tempo trascorso secondi = " + seconds);
+//        System.out.println("Tempo trascorso minuti = " + seconds / 60);
+//
+//    }
+
+}
