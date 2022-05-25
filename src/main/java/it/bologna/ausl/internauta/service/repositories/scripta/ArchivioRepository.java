31b295645d9c0f32cfc85601931b4d982b8a730f
D'Onza Michele <m.donza@nextsw.it>
2022-05-25 17:58:44 +0200

    aggiunta projecton

--- src/main/java/it/bologna/ausl/internauta/service/repositories/scripta/ArchivioRepository.java
+++ src/main/java/it/bologna/ausl/internauta/service/repositories/scripta/ArchivioRepository.java
@@ -7,6 +7,8 @@ import it.nextsw.common.annotations.NextSdrRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import it.nextsw.common.repositories.NextSdrQueryDslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 /**
  * per convenzione nostra, collectionResourceRel e path devono avere lo stesso
@@ -17,4 +19,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ArchivioRepository extends
         NextSdrQueryDslRepository<Archivio, Integer, QArchivio>,
         JpaRepository<Archivio, Integer> {
+    
+    @Procedure("permessi.calcola_permessi_espliciti")
+    public void calcolaPermessiEspliciti(
+        @Param("id_archivio") Integer idArchivio
+    );
 }
