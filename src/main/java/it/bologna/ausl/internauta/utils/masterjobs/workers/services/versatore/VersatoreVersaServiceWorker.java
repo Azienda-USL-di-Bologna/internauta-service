2349c003c7492303b0a3807d34f3d7da094e247a
g.demarco <giuseppe.demarco@nextsw.it>
2022-12-15 10:58:37 +0100

    separati i servizi per il versamento e il controllo dei versamenti

---
+++ src/main/java/it/bologna/ausl/internauta/utils/masterjobs/workers/services/versatore/VersatoreVersaServiceWorker.java
@@ -0,0 +1,53 @@
+package it.bologna.ausl.internauta.utils.masterjobs.workers.services.versatore;
+
+import it.bologna.ausl.internauta.service.utils.CachedEntities;
+import it.bologna.ausl.internauta.utils.masterjobs.annotations.MasterjobsWorker;
+import it.bologna.ausl.internauta.utils.masterjobs.exceptions.MasterjobsWorkerException;
+import it.bologna.ausl.internauta.utils.masterjobs.workers.WorkerResult;
+import it.bologna.ausl.internauta.utils.masterjobs.workers.services.ServiceWorker;
+import it.bologna.ausl.internauta.utils.masterjobs.workers.services.VersatoreServiceCore;
+import it.bologna.ausl.internauta.utils.parameters.manager.ParametriAziendeReader;
+import it.bologna.ausl.model.entities.configurazione.Applicazione;
+import it.bologna.ausl.model.entities.versatore.SessioneVersamento;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author gdm
+ * 
+ * Classe che implementa il servizio del versamento del versatore.
+ * Si occupa di leggere i parametri da configurazione.parametri_azienda e richiamare il core del servizio 
+ * (la parte in cui si accodano effettivamente i mestieri di versamento) che è definita nella classe VersatoreServiceCore
+ */
+@MasterjobsWorker
+public class VersatoreVersaServiceWorker extends ServiceWorker {
+    private static Logger log = LoggerFactory.getLogger(VersatoreVersaServiceWorker.class);
+    
+    private String name = VersatoreVersaServiceWorker.class.getSimpleName();
+    
+    @Autowired
+    private CachedEntities cachedEntities;
+    
+    @Autowired
+    private ParametriAziendeReader parametriAziendaReader;
+    
+    @Override
+    public String getName() {
+        return this.name;
+    }
+    
+    @Override
+    public WorkerResult doWork() throws MasterjobsWorkerException {
+        
+        Map<Integer, Map<String, Object>> aziendeAttiveConParametri = VersatoreServiceUtils.getAziendeAttiveConParametri(parametriAziendaReader, cachedEntities);
+        
+        // richiama il metodo del core per effettuare l'accodamento del mestiere di verrsamento
+        VersatoreServiceCore versatoreServiceCore = new VersatoreServiceCore(masterjobsJobsQueuer, masterjobsObjectsFactory);
+        versatoreServiceCore.queueVersatoreJobs(SessioneVersamento.AzioneVersamento.VERSAMENTO, 
+                aziendeAttiveConParametri, Applicazione.Applicazioni.scripta.toString());
+        return null;
+    }
+}
