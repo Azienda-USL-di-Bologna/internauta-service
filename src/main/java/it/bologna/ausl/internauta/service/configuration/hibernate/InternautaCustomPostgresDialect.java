d3da968e2f157dcce19d95bba156b12c60d61d28
gdm <giuseppe.demarco@nextsw.it>
2023-08-31 15:58:26 +0200

    aggiunta del mapping di del tipo StringArrayType nel custom dialect di postgres

--- src/main/java/it/bologna/ausl/internauta/service/configuration/hibernate/InternautaCustomPostgresDialect.java
+++ src/main/java/it/bologna/ausl/internauta/service/configuration/hibernate/InternautaCustomPostgresDialect.java
@@ -1,5 +1,6 @@
 package it.bologna.ausl.internauta.service.configuration.hibernate;
 
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
 import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
 import it.bologna.ausl.internauta.utils.jpa.tools.GenericArrayUserType;
 import it.nextsw.common.dialect.CustomPostgresDialect;
@@ -18,6 +19,6 @@ public class InternautaCustomPostgresDialect extends CustomPostgresDialect {
         
         this.registerColumnType(Types.OTHER, JsonNodeBinaryType.class.getName());
         registerHibernateType(Types.OTHER, JsonNodeBinaryType.class.getName());
-
+        this.registerHibernateType(2003, StringArrayType.class.getName());
     }
 }
