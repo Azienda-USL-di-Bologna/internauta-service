79afff145cecdb864489d00e25b98fd7a61a9b92
QB <m.venturi@dilaxia.com>
2023-10-06 11:03:12 +0200

    Merge branch 'feature/BCD-139' into sviluppo
    (cherry picked from commit 8ea3fa0ea6d0d980ffc02c899ddb1a1bdaa95fe7)

--- src/main/java/it/bologna/ausl/model/entities/logs/projections/KrintInformazioniUtente.java
+++ src/main/java/it/bologna/ausl/model/entities/logs/projections/KrintInformazioniUtente.java
@@ -1,27 +1,73 @@
-
 package it.bologna.ausl.model.entities.logs.projections;
 
+import it.bologna.ausl.internauta.model.bds.types.PermessoEntitaStoredProcedure;
+import it.bologna.ausl.model.entities.baborg.Ruolo;
 import it.bologna.ausl.model.entities.baborg.Utente;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.data.rest.core.config.Projection;
 
 
 @Projection(name = "KrintInformazioniUtente", types = Utente.class)
 public interface KrintInformazioniUtente {
-      
-    // azienda
-    @Value("#{@userInfoService.getAziendaKrint(target)}") 
-    KrintBaborgAzienda getAzienda();
-            
-    // strutture
-    @Value("#{@userInfoService.getStruttureKrint(target)}")
-    List<KrintBaborgUtenteStruttura> getStrutture();
+    
+    @Value("#{target.getId()}") 
+    Integer getId();
+    
+    @Value("#{target.getFax()}") 
+    String getFax();
+    
+    @Value("#{target.getAttivo()}") 
+    Boolean getAttivo();
+    
+    @Value("#{target.getEmails()}") 
+    String[] getEmails();
+    
+    @Value("#{target.getVersion()}") 
+    ZonedDateTime getVersion();
+    
+    @Value("#{target.getBitRuoli()}") 
+    Integer getBitRuoli();
+    
+    @Value("#{target.getOmonimia()}") 
+    Boolean getOmonimia();
+    
+    @Value("#{target.getTelefono()}") 
+    String getTelefono();
+    
+    @Value("#{target.getUsername()}") 
+    String getUsername(); 
+    
+    @Value("#{target.getIdAzienda().getId()}") 
+    Integer getIdAzienda();
     
     // persona
-    @Value("#{@userInfoService.getPersonaKrint(target)}")
-    KrintBaborgPersona getIdPersona();                 
+    @Value("#{@userInfoService.getInformazioniPersonaKrint(target)}")
+    KrintInformazioniPersona getIdPersona();
     
-  
+    @Value("#{target.getMappaRuoli()}") 
+    Map<String, List<Ruolo>> getMappaRuoli();
+    
+    @Value("#{target.getIdInquadramento()}") 
+    String getIdInquadramento();
+    
+    @Value("#{target.getPermessiDiFlusso()}") 
+    List<PermessoEntitaStoredProcedure> getPermessiDiFlusso();
+    
+    @Value("#{target.getEntityDescription()}") 
+    String getEntityDescription();
+    
+    @Value("#{target.getRuoliUtentiPersona()}") 
+    Map<String, Map<String, List<String>>> getRuoliUtentiPersona();
+    
+    @Value("#{target.getStruttureDelSegretario()}") 
+    Integer[] getStruttureDelSegretario();
+    
+    @Value("#{target.getPermessiDiFlussoByIdUtente()}") 
+    List<PermessoEntitaStoredProcedure> getPermessiDiFlussoByIdUtente();
+    
+    @Value("#{target.getPermessiDiFlussoByCodiceAzienda()}") 
+    Map<String, List<PermessoEntitaStoredProcedure>> getPermessiDiFlussoByCodiceAzienda();
 }
