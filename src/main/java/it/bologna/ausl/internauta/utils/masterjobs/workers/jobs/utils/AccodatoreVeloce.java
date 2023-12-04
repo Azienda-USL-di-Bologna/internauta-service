b97f7d6d645edc215b40b8a2edf1b790dd6a3538
g.demarco <giuseppe.demarco@nextsw.it>
2023-12-04 17:59:25 +0100

    rimesso skipIfAlreadyPresent(true)

--- src/main/java/it/bologna/ausl/internauta/utils/masterjobs/workers/jobs/utils/AccodatoreVeloce.java
+++ src/main/java/it/bologna/ausl/internauta/utils/masterjobs/workers/jobs/utils/AccodatoreVeloce.java
@@ -55,7 +55,7 @@ public class AccodatoreVeloce {
                     .newBuilder()
                     .addWorker(worker)
                     .waitForObject(false)
-                    .skipIfAlreadyPresent(false)
+                    .skipIfAlreadyPresent(true)
                     .priority(SetPriority.NORMAL)
                     .build()
             );
